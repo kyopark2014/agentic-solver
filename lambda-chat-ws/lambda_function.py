904daf318842110483e891cffde9a7ba3eed9133
Kyoungsu Park <kyopark2014@gmail.com>
2024-11-22 08:02:27 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -2373,20 +2373,21 @@ def solve_CSAT_Korean(connectionId, requestId, paragraph, question, question_plu
                 
         list_choices = ""
         for i, choice in enumerate(choices):
-            list_choices += f"{i+1}. {choice}\n"
+            list_choices += f"({i+1}) {choice}\n"
         print('list_choices: ', list_choices)    
         
         update_state_message("planning...", config)
         
         if isKorean(question)==True:            
             system = (
-                "당신은 수능 국어 문제를 잘푸는 일타강사입니다."
+                "당신은 복잡한 문제를 해결하기 위해 step by step plan을 생성하는 AI agent입니다."
                 "<paragraph> tag의 내용을 참조하여 <question> tag의 질문에 대한 적절한 답변을 <choice> tag안에서 선택하가 위한 단계별 계획을 세우세요."
                 # "이 계획은 답변을 구하기 위한 단계를 포함합니다. 이를 올바르게 실행하면 정확한 답을 얻을 수 있습니다. 불필요한 단계는 추가하지 마십시오."
-                "This plan should involve individual tasks, that if executed correctly will yield the correct answer. Do not add any superfluous steps."
-                "The result of the final step should be the final answer. Make sure that each step has all the information needed - do not skip steps."                
+                #"This plan should involve individual tasks, that if executed correctly will yield the correct answer. Do not add any superfluous steps."
+                #"The result of the final step should be the final answer. Make sure that each step has all the information needed - do not skip steps."                
                 
                 "다음 형식으로 단계별 계획을 세웁니다."
+                "각 단계는 반드시 하나의 문장으로 AI agent가 수행할 내용을 명확히 나타냅니다."
                 "1. [질문을 해결하기 위한 단계]"
                 "2. [질문을 해결하기 위한 단계]"
                 "..."
@@ -2508,7 +2509,7 @@ def solve_CSAT_Korean(connectionId, requestId, paragraph, question, question_plu
                 
         list_choices = ""
         for i, choice in enumerate(choices):
-            list_choices += f"{i+1}. {choice}\n"
+            list_choices += f"({i+1}) {choice}\n"
         print('list_choices: ', list_choices)    
         
         update_state_message("replanning...", config)
@@ -3136,7 +3137,7 @@ def getResponse(connectionId, jsonBody):
                 for i, problem in enumerate(problems):
                     print(f'preoblem[{i}]: {problem}')
                 
-                n = 1
+                n = 2
                 question = problems[n]["question"]
                 print('question: ', question)                
                 question_plus = ""
