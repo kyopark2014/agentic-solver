1e6fff5607f429d2ac3c270cd26adeb12f46cd0e
Kyoungsu Park <kyopark2014@gmail.com>
2024-11-23 17:55:36 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -3420,7 +3420,7 @@ def getResponse(connectionId, jsonBody):
                 #for i, data in enumerate(json_data):
                 #    print(f'index: {i}: {json.dumps(data)}')
                     
-                idx = 5
+                idx = 6
                 question_group = json_data[idx]
                 paragraph = question_group["paragraph"]
                 print('paragraph: ', paragraph)
